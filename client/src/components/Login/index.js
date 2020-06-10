573680f68a0b1114707115a7a1e7aca3055d30c4
Svyatoslav Vyshnevetskiy <vyshnevetskyi@gmail.com>
2020-06-10 16:46:16 +0200

    login page redesign

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,14 +1,46 @@
-import React from 'react'
+import React from "react"
 
-import './login.scss'
+import "./login.scss"
 
-function Login () {
+function Login() {
     return (
         <div className="login">
             <div className="login-content">
-                <h2 className="header"> Welcome to the Music Hoarder </h2>
-                <p className="info">For full experience you need to have Spotify Premium account.</p>
-                <a href="http://localhost:8888/login" className="login-button">Login with Spotify</a>
+                <div className="auth-section">
+                    <h2>Welcome</h2>
+                    <p>To The Music Hoarder!</p>
+                    <a
+                        href="http://localhost:8888/login"
+                        className="login-button"
+                    >
+                        Login with Spotify
+                    </a>
+                </div>
+                <div className="explain-section">
+                    <ul className="feature-list">
+                        <li className="feature-item">
+                            This application is based on Spotify Web API.
+                        </li>
+                        <li className="feature-item">
+                            For full expirience you need to have Spotify premium
+                            account.
+                        </li>
+                        <li className="feature-item">
+                            Authorization is implemented with Spotify OAuth2
+                            protocol.
+                        </li>
+                        <li className="feature-item">
+                            You can search tracks using advanced search
+                            criterias.
+                        </li>
+                        <li className="feature-item">
+                            You can search tracks by genres.
+                        </li>
+                        <li className="feature-item">
+                            You can save tracks to your library.
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
